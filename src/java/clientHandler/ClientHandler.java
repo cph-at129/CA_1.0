a59c1b974a1a70456fea84e94b5c64cc9b887c73
Koziar <lkoziars@gmail.com>
2015-09-09 21:33:54 +0200

    ClientHandler and userList's classes
    ClientHandler class, its run() (not done yet) and methods for  dealing
    with userList like:
    sendUserList(Socket clientSocket),
    getUserNameAndUpdateUserList(String inputLine, ClientHandler aThis),
    sendUpdatedUserList()

--- src/java/clientHandler/ClientHandler.java
+++ src/java/clientHandler/ClientHandler.java
@@ -5,10 +5,61 @@
  */
 package clientHandler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import server.Server;
+
 /**
  *
  * @author Aleksandar, Lukasz, Viktor
  */
-public class ClientHandler {
+public class ClientHandler implements Runnable
+{
+
+    public static final String USER = "USER#";
+    public static final String MSG = "MSG#";
+    public static final String STOP = "STOP#";
+    public static final String USERLIST = "USERLIST#";
+
+    private final Server server;
+    private final Socket clientSocket;
     
+    public ClientHandler(Socket clientSocket, Server server)
+    {
+
+        this.server = server;
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            server.sendUserList(clientSocket);
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+            String outputLine;
+            while ((inputLine = in.readLine()) != null) { //loop to get the user name and send it to everyone
+                boolean status = server.getUserNameAndUpdateUserList(inputLine, this);
+                if (status) {
+                    server.sendUpdatedUserList();
+                    break;
+                }
+            }
+            out.println("You are online");
+
+            clientSocket.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/java/server/Server.java
+++ src/java/server/Server.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package server;
 
 import clientHandler.ClientHandler;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,13 +19,20 @@ import java.util.logging.Logger;
  *
  * @author Aleksandar, Lukasz, Viktor
  */
-public class Server {
+public class Server
+{
 
     private static final int portNumber = 8112;//server's port number
     private static final String localhost = "localhost";
     private static HashMap<String, ClientHandler> userList = new HashMap();//save the clients
 
-    public void connect() {
+    public static final String USER = "USER#";
+    public static final String MSG = "MSG#";
+    public static final String STOP = "STOP#";
+    public static final String USERLIST = "USERLIST#";
+
+    public void connect()
+    {
 
         try {
             ServerSocket serverSocket = new ServerSocket();
@@ -34,14 +43,28 @@ public class Server {
             }
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-        
+
         }
     }
-    
-    
-    public static void main(String[] args) {
-        
-        
-        
+
+    public static void main(String[] args)
+    {
+
     }
+
+    public void sendUserList(Socket clientSocket)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean getUserNameAndUpdateUserList(String inputLine, ClientHandler aThis)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void sendUpdatedUserList()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
